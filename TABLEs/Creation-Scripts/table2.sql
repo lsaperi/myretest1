52af8651af24b5704bcd7edbeab966355b82bbb2
lsaperi <lsaperi>
2025-07-08 09:52:35 -0300

    tabke

--- TABLEs/Creation-Scripts/table2.sql
+++ TABLEs/Creation-Scripts/table2.sql
@@ -1,8 +1,6 @@
 CREATE TABLE `table2` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
   `table1_id` int DEFAULT NULL,
-  `notes` text,
-  PRIMARY KEY (`id`),
-  KEY `fk_table2_table1` (`table1_id`),
-  CONSTRAINT `fk_table2_table1` FOREIGN KEY (`table1_id`) REFERENCES `table1` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
