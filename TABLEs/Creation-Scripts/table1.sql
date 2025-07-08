0f1f752d4e700cf8650d54141d3d9aa0e936064f
lsaperi <lsaperi>
2025-07-08 10:00:34 -0300

    ttre

--- TABLEs/Creation-Scripts/table1.sql
+++ TABLEs/Creation-Scripts/table1.sql
@@ -2,5 +2,7 @@ CREATE TABLE `table1` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `table2_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_table1_table2` (`table2_id`),
+  CONSTRAINT `fk_table1_table2` FOREIGN KEY (`table2_id`) REFERENCES `table2` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- TABLEs/Creation-Scripts/table2.sql
+++ TABLEs/Creation-Scripts/table2.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `table2` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) DEFAULT NULL,
   `table1_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `notes` text,
+  PRIMARY KEY (`id`),
+  KEY `fk_table2_table1` (`table1_id`),
+  CONSTRAINT `fk_table2_table1` FOREIGN KEY (`table1_id`) REFERENCES `table1` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
